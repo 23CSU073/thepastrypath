3ca0a4e1d72535ba1976ca0f95e5c2560b69506c
Ajitesh Sharma <ajitesh.sharma@ourbus.com>
2026-05-07 23:13:29 +0530

    added groq API key

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -3,5 +3,5 @@ class AppConstants {
   static const moods = ['Cozy Study Cafe', 'Date Spot', 'Family Bakery', 'Late Night Coffee'];
   static const gurugramLatitude = 28.4595;
   static const gurugramLongitude = 77.0266;
-  static const groqApiKey = '';
+  static const groqApiKey = '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';
 }
